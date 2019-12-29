bb681eac3ae025ef32c232bdce4841cc988e104d
watercam <53884012+watercam@users.noreply.github.com>
2019-12-29 13:52:21 -0500

    Add files via upload

---
+++ 1105graphing a pegged chart.R
@@ -0,0 +1,77 @@
+#####################################
+###### graphing a pegged chart ######
+#####################################
+
+install.packages("quantmod")
+install.packages("xts")
+library(quantmod)
+library(xts)
+
+## get data from yahoo
+jnj <- getSymbols("JNJ", src = "yahoo", auto.assign = FALSE)
+gspc <- getSymbols("^GSPC", src = "yahoo", auto.assign = FALSE)
+pfe <- getSymbols("PFE", src = "yahoo", auto.assign = FALSE)
+
+## create subsets of data
+jnj_subset <- subset(jnj[ , 6], index(jnj) >= "2010-01-01" & 
+                       index(jnj) <= "2015-12-31")
+gspc_subset <- subset(gspc[ , 6], index(gspc) >= "2010-01-01" & 
+                        index(gspc) <= "2015-12-31")
+pfe_subset <- subset(pfe[ , 6], index(pfe) >= "2010-01-01" & 
+                       index(pfe) <= "2015-12-31")
+
+## merge data into one database
+all_adjusted <- na.locf(merge(pfe_subset, jnj_subset))
+all_adjusted <- na.locf(merge(all_adjusted, gspc_subset))
+
+## normalize data, convert from xts to dataframe
+#remove na.omit values from dataset
+all_adjusted_nona <- na.omit(all_adjusted)
+#convert dates into vector
+date <- index(all_adjusted_nona)
+#combine dates vector and dataset
+all_adjusted_df <- cbind(data.frame(date), data.frame(all_adjusted_nona))
+
+## calculate normalized values by referencing row 1 for each asset
+all_adjusted_df$JNJ.Norm <- all_adjusted_df$JNJ.Adjusted / 
+  all_adjusted_df$JNJ.Adjusted[1]
+all_adjusted_df$GSPC.Norm <- all_adjusted_df$GSPC.Adjusted / 
+  all_adjusted_df$GSPC.Adjusted[1]
+all_adjusted_df$PFE.Norm <- all_adjusted_df$PFE.Adjusted / 
+  all_adjusted_df$PFE.Adjusted[1]
+
+## plot values
+plot( x = date, xlab = "Date",
+      y = all_adjusted_df$JNJ.Norm, ylab = "Normalized Return (%)",
+      ylim = c(.5,2.5), type = "l",
+      col = "blueviolet", lwd = 2, lty = 1,
+      main = "Johnson and Johnson (JNJ), SP 500 (GPSC), Pfizer (PFE)
+Normalized Return from Adjusted Close
+January 1, 2010 - December 31, 2015")
+
+## add gspc and pfe lines
+lines( x = date, y = all_adjusted_df$GSPC.Norm, lwd = 2, lty = 2,
+       col = "forestgreen")
+lines( x = date, y = all_adjusted_df$PFE.Norm, lwd = 2, lty = 3,
+       col = "red")
+
+## add legend
+legend( "bottomright", c("JNJ", "GSPC", "PFE"),
+        col = c("blueviolet","forestgreen", "red"),
+        lwd = c(2, 2, 2), lty = c(1, 2, 3))
+
+## alternate graph with horizontal line
+plot ( x = all_adjusted_df$date, xlab = "Date",
+       y = all_adjusted_df$JNJ.Norm, ylab = "Value of $1 investment ($)",
+       type = "l", ylim = c(0, 2.5), lwd = 2, lty = 2, col = "blueviolet",
+       main = "Johnson and Johnson (JNJ), Pfizer (PFE), SP 500 (GSPC)
+       Value of $1 Investment Based on Daily Adjusted CLsoe Prices
+       January 1, 2010 - December 31, 2015")
+lines( x = all_adjusted_df$date, y = all_adjusted_df$GSPC.Norm,
+       lwd = 2, lty = 3, col = "red")
+lines( x = all_adjusted_df$date, y = all_adjusted_df$PFE.Norm,
+       lwd = 2, lty = 4, col = "forestgreen")
+legend("topleft", c("JNJ", "GSPC", "PFE"), lwd = c(2, 2, 2),
+       lty = c(2, 3, 4), col = c("blueviolet", "red", "forestgreen"))
+abline( h = 1, lty = 1, col = "black")
+
---
+++ 1114calculating regression.R
@@ -0,0 +1,65 @@
+####################################
+### calculating regression #########
+### 11/14 lecture ##################
+####################################
+
+install.packages("quantmod")
+install.packages("xts")
+library(quantmod)
+library(xts)
+
+load( file = "peer_all_df.Rdata")
+
+load( file = "regression_dataset.Rdata")
+
+## download exchange rate data
+usd.eur <- getSymbols("DEXUSEU", src = "FRED", auto.assign = FALSE)
+usd.eur <- na.omit(usd.eur)
+
+## calculate daily % change
+usd.eur$eur.change <- Delt(usd.eur$DEXUSEU) * 100
+
+#convert to dataframe and attach to regression_dataset; limit to variables of interest
+date <- index(usd.eur)
+usd.eur.df <- cbind(data.frame(date), data.frame(usd.eur))
+regression_data_all <- merge(regression_dataset, usd.eur.df)
+regression_data_vars <- regression_data_all[ , c(1,5,6,11,12,14)]
+
+## create dataset that subsets down to estimation and event window
+event_study <- subset(regression_data_vars, regression_data_vars$date >=
+                      "2014-01-19" & regression_data_vars$date <= "2015-01-20")
+
+## create one subset that only includes estimation window (drop last event)
+es <- subset(event_study, event_study$date < "2015-01-20")
+
+## run 1 factor regression; store in market_model
+market_model <- lm( es$JNJ.Ret ~ es$GSPC.Ret)
+summary(market_model) #prints results of OLS regression
+
+## run 3 factor regression; store in threefactor_model
+threefactor_model <- lm(es$JNJ.Ret ~ es$GSPC.Ret + es$EW.Ret + es$eur.change)
+summary(threefactor_model) #prints results of OLS regression
+
+## run 3 factor regression (VW instead of EW); store in threefactor_model
+threefactor_modelVW <- lm(es$JNJ.Ret ~ es$GSPC.Ret + es$VW.Ret + es$eur.change)
+summary(threefactor_modelVW) #prints results of OLS regression
+
+## add predicted returns from both models to event_study 
+event_study$JNJ.Ret.Pred.M <- summary(market_model)$coefficients[1] +
+  (summary(market_model)$coefficients[2] * event_study$GSPC.Ret)
+event_study$JNJ.Ret.Pred.3F <- summary(threefactor_model)$coefficients[1] +
+  (summary(threefactor_model)$coefficients[2] * event_study$GSPC.Ret) +
+  (summary(threefactor_model)$coefficients[3] * event_study$EW.Ret) +
+  (summary(threefactor_model)$coefficients[4] * event_study$eur.change)
+
+## calculate abnormal return (actual - expected)
+event_study$JNJ.Ab.M <- event_study$JNJ.Ret - event_study$JNJ.Ret.Pred.M
+event_study$JNJ.Ab.3F <- event_study$JNJ.Ret - event_study$JNJ.Ret.Pred.3F
+
+## add t stat to event_study; tells the stat significance of the abnormal return
+event_study$tstat.M <- event_study$JNJ.Ab.M / summary(market_model)$sigma
+event_study$tstat.3F <- event_study$JNJ.Ab.3F / summary(threefactor_model)$sigma
+
+## what happened on the day of event?
+tail(event_study, n = 1)
+
